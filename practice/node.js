5232e3029a5c527a054034522625ec08bd31243c
Guy Wassather <ggwass@gmail.com>
2018-10-02 18:10:47 -0700

    Move Node to own class

---
+++ practice/node.js
@@ -0,0 +1,13 @@
+/*
+ The node class is a simple object. It's constructor method sets the data/ value
+ of the node, as well as sets the next value, the value it holds a pointer to.
+ */
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// export default Node;
+module.exports = Node;
