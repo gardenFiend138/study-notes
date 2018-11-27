cca326275bb9346959ff1bd2dc099bbec73efacd
Guy Wassather <ggwass@gmail.com>
2018-11-27 08:05:06 -0800

    Finish `find interesection point of two linked lists`

---
+++ practice/ll-intersection.js
@@ -0,0 +1,39 @@
+const LinkedList = require('./linked-list/singly-linked/linked-lists.js');
+
+const makeList = (arr) => {
+  const ll = new LinkedList();
+  ll.add(arr.shift());
+  arr.forEach(num => ll.add(num));
+  return ll;
+}
+
+const findIntersection = (list1, list2) => {
+  const seen = {};
+  const [shortest, longest] = list1.length > list2.length
+    ? [list2, list1]
+    : [list1, list2];
+  let node = shortest.head;
+
+  while (node) {
+    seen[node.data] = true;
+    node = node.next;
+  }
+
+  node = longest.head;
+
+  while (node) {
+    if (seen[node.data]) return node.data;
+    node = node.next;
+  }
+
+  return null;
+}
+
+const list_1 = makeList([1,2,3,4,5,6,7,8]);
+const list_2 = makeList([15,14,13,12,11,10,9,8]);
+const list_3 = makeList([12,11,13,14,15,138,22,21,24]);
+const list_4 = makeList([1,2,3,4,138,4,3,2]);
+
+console.log('find intersection: 8', findIntersection(list_1, list_2));
+console.log('find intersection: 138', findIntersection(list_3, list_4));
+console.log('find intersection: none', findIntersection(list_1, list_3));
